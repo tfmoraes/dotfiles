45ffce46096415b0e0196e43eaf3672663313b26
Thiago Franco de Moraes <totonixsame@gmail.com>
2020-06-24 15:38:55 -0300

    nixos with neovim and some pyhton packages

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,21 +1,6 @@
 { pkgs, ... }:
 
 {
-    time.timeZone = "America/Sao_Paulo";
-
-    packageOverrides = pkgs: with pkgs; {
-        neovim = pkgs.neovim.override {
-            extraPython3Packages = (ps: with ps; [
-                    neovim
-                    black
-                    isort
-                    jedi
-                    pylint
-            ]);
-            extraPythonPackages = (ps: with ps; [
-                    neovim
-                    jedi
-            ]);
-        };
-    };
+  allowUnfree = true;
+  time.timeZone = "America/Sao_Paulo";
 }
